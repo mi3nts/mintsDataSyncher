f32214b238d250e0503c3970a4e6b2915b84a6f1
lakihaomal <lhw150030@utdallas.edu>
2024-07-12 17:51:57 -0500

    Added syncers for all nodes

--- dataSyncherV2RawAllSensors.py
+++ dataSyncherV2RawAllSensors.py
@@ -65,21 +65,18 @@ if __name__ == "__main__":
             currentDateStr = currentDate.strftime("%Y_%m_%d")
             currentDate   += delta
 
-            for sensorID in sensorIDs:
-                print("========================SENSORS========================")
-                print("Syncing data from node " + nodeID + ", sensor ID " + sensorID +  " for the date of " + currentDateStr)
-                includeStatement = "--include='*"+  sensorID + "_" + currentDateStr +".csv' "
-                includeStatements = includeStatements + includeStatement;
-                    
+            print("Syncing data from node " + nodeID +" for the date of " + currentDateStr)
+            includeStatement = "--include='*"+ "_" + currentDateStr +".csv' "
+            includeStatements = includeStatements + includeStatement;
+                
         sysStr = 'rsync -avzrtum -e "ssh -p 2222" ' +  includeStatements+ "--include='*/' --exclude='*' mints@mintsdata.utdallas.edu:/mfs/io/groups/lary/mintsData/raw/" + nodeID + " " + dataFolder
+        print(sysStr)
+        # os.system(sysStr)
+
+        sysStr = 'rsync -avzrtum -e "ssh -p 2222" ' +  includeStatements+ "--include='*/' --exclude='*' mints@mintsdata.utdallas.edu:/mfs/io/groups/lary/gitHubRepos/raw/" + nodeID + " " + dataFolder
         print(sysStr)
         # os.system(sysStr)
 
         sysStr = 'rsync -avzrtum -e "ssh -p 2222" ' +  includeStatements+ "--include='*/' --exclude='*' mints@mintsdata.utdallas.edu:/home/mints/raw/" + nodeID + " " + dataFolder
         print(sysStr)
         # os.system(sysStr)
-
-        sysStr = 'rsync -avzrtum -e "ssh -p 2222" ' +  includeStatements+ "--include='*/' --exclude='*' mints@mintsdata.utdallas.edu:/mfs/io/groups/lary/gitHubRepos/raw" + nodeID + " " + dataFolderMqtt
-        print(sysStr)
-        # os.system(sysStr)
-
